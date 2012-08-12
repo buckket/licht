ac4d13e38c815c2b5b01d63f18f7eb3761cef008
teddydestodes <derkomtur@schattengang.net>
2012-08-13 01:40:28 +0200

    penis, lol

---
+++ client.py
@@ -0,0 +1,41 @@
+'''
+Created on Aug 13, 2012
+
+@author: teddydestodes
+'''
+import socket
+import struct
+import pickle
+import argparse
+class LichtClient(object):
+    
+    def __init__(self,address, port):
+        self.sock = socket.socket( socket.AF_INET, # Internet
+                              socket.SOCK_DGRAM ) # UDP
+        self.address = address
+        self.port = port
+    
+    def sendCommand(self, command, params):
+        self.sock.sendto( pickle.dumps([command,params]), (self.address, self.port) )
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Lichterkette!!!!',
+                                     epilog='Penis, LOL!')
+    subparsers = parser.add_subparsers(dest='command', help='sub-command help')
+    authparser = subparsers.add_parser('fade', help='fades color')
+    authparser.add_argument('r')
+    authparser.add_argument('g')
+    authparser.add_argument('b')
+    offparser = subparsers.add_parser('off', help='turns all off')
+    nightparser = subparsers.add_parser('night', help='turns on night mode')
+    turingparser = subparsers.add_parser('turing', help='turns on turing mode')
+    args = parser.parse_args()
+    client = LichtClient('192.168.2.145', 16321)
+    if args.command == 'off':
+        client.sendCommand(3, ((0,0,0)))
+    elif args.command == 'turing':
+        client.sendCommand(3, ((0xff,0,0xff)))
+    elif args.command == 'night':
+        client.sendCommand(3, ((0x10,0,0)))
+    elif args.command == 'fade':
+        client.sendCommand(3, ((int(args.r),int(args.g),int(args.b))))
---
+++ server.py
@@ -0,0 +1,117 @@
+'''
+Created on Aug 13, 2012
+
+@author: teddydestodes
+'''
+
+import socket, time, struct
+import threading
+import pickle
+import spidev
+
+class RefreshThread(threading.Thread):
+    SPILock = threading.Lock()
+    
+    def __init__(self, length):
+        threading.Thread.__init__(self)
+        self.buffer = []
+        self.length = length
+        for i in range(0,length*3):
+            self.buffer.append(0)
+        self.spi = spidev.SpiDev(2,0)
+        self.spi.max_speed_hz = 1000000
+    
+    def run(self):
+        while True:
+            self.flush_buffer()
+            time.sleep(.5)
+            
+    def flush_buffer(self):
+        RefreshThread.SPILock.acquire()
+        self.spi.writebytes(self.buff)
+        time.sleep(0.0005)
+        RefreshThread.SPILock.release()
+        
+    def set_color(self, color, pixel=None, flush=True):
+        if pixel != None:
+            self.buffer[pixel*3]   = color[0]
+            self.buffer[pixel*3+1] = color[1]
+            self.buffer[pixel*3+2] = color[2]
+        else:
+            for p in range(0,self.length):
+                self.buffer[p*3] = color[0]
+                self.buffer[p*3+1] = color[1]
+                self.buffer[p*3+2] = color[2]
+        if flush:
+            self.flush_buffer()
+
+    def push_color(self, color):
+        self.buffer.insert(0,color[2])
+        self.buffer.insert(0,color[1])
+        self.buffer.insert(0,color[0])
+        self.buffer.pop()
+        self.buffer.pop()
+        self.buffer.pop()
+        self.flush_buffer()
+        
+    def fade(self, color):
+        steps = 50
+        for s in range(0,steps):
+            for p in range(0, self.length):
+                self.set_color(self.avg_color(self.get_color(p), color, steps-s), p, flush=False)
+            self.flush_buffer()
+        self.set_color(color)
+        
+
+    def get_color(self, pixel):
+        return [self.buffer[pixel*3],self.buffer[pixel*3+1],self.buffer[pixel*3+2]]
+
+    def avg_color(self, color, color2, weight=1):
+        return ((color[0]*weight+color2[0])/(1+weight),(color[1]*weight+color2[1])/(1+weight),(color[2]*weight+color2[2])/(1+weight))
+
+class RecieveThread(threading.Thread):
+    
+    def __init__(self,address,port, refresh):
+        threading.Thread.__init__(self)
+        self.sock = socket.socket( socket.AF_INET,
+                                   socket.SOCK_DGRAM )
+        self.sock.bind( (address,port) )
+        self.rthread = refresh
+    
+    def run(self):
+        while True:
+            data, addr = self.sock.recvfrom( 1024 ) # buffer size is 1024 bytes
+            t = pickle.loads(data)
+            if t[0] == 0:
+                self.command_off()
+            if t[0] == 1:
+                self.command_setcolor(t[1])
+            if t[0] == 2:
+                self.command_pushcolor(t[1])
+            if t[0] == 3:
+                self.command_fade(t[1])
+    def command_off(self):
+        self.rthread.set_color((0,0,0))
+    def command_setcolor(self, param):
+        self.rthread.set_color(param[0],param[1])
+    def command_pushcolor(self, param):
+        self.rthread.push_color(param[0])
+    def command_fade(self, param):
+        self.rthread.fade(param)
+class LichtServer(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,address,port, size):
+        '''
+        Constructor
+        '''
+        self.refresh_thread = RefreshThread(size)
+        self.recieve_thread = RecieveThread(address,port, self.refresh_thread)
+        self.recieve_thread.start() 
+        self.refresh_thread.start()
+        
+                
+server = LichtServer('localhost',16321, 25)
